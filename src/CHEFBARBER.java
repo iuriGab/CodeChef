b18eb4f59a614ea382e2e9c739ab989e2c282aa4
Iuri Gabriel <iuri_gabriel@hotmail.com>
2022-03-04 12:14:25 -0300

    Initial commit

---
+++ src/CHEFBARBER.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CHEFBARBER {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int times = sc.nextInt();
+        int i = 0;
+        while (i < times){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = a * b;
+
+            System.out.println(result);
+            i++;
+        }
+    }
+}
