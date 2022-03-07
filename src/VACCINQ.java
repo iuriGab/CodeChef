736fbbf8e62bc8f71a23fc88363c651619718c9f
Iuri Gabriel <iuri_gabriel@hotmail.com>
2022-03-07 15:55:34 -0300

    update!

--- src/VACCINQ.java
+++ src/VACCINQ.java
@@ -2,7 +2,7 @@
 //position from the front of the queue. It takes X minutes to vaccinate a
 //child and Y minutes to vaccinate an elderly person. Assume Chef is
 //an elderly person.
-//
+
 //You are given a binary array A1,A2,…,AN of length N, where Ai=1 denotes
 //there is an elderly person standing on the ith position of the queue,
 //Ai=0 denotes there is a child standing on the ith position of the queue.
@@ -17,15 +17,19 @@ public class VACCINQ {
         Scanner sc = new Scanner(System.in);
         
         for (int i= sc.nextInt(); i>0; i--){
+
+            int result = 0;
+
             int lengthQueue = sc.nextInt();
             int positionChef = sc.nextInt();
             int minutesChild = sc.nextInt();
             int minutesElderly = sc.nextInt();
-            int result = 0;
+
             int[] array = new int[lengthQueue];
             for (int index = 0; index < lengthQueue; index++) {
                 array[index] = sc.nextInt();
             }
+
             for (int index2 = 0; index2 < positionChef; index2++) {
                 if (array[index2] == 0){
                     result += minutesChild;
@@ -33,6 +37,7 @@ public class VACCINQ {
                     result += minutesElderly;
                 }
             }
+
             System.out.println(result);
         }
     }
