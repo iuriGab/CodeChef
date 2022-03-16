a865c5879fc6707758a0d89f71ef7dc5a92d75ef
Iuri Gabriel <iuri_gabriel@hotmail.com>
2022-03-16 18:29:03 -0300

    update!

--- src/CHEFBARBER.java
+++ src/CHEFBARBER.java
@@ -7,9 +7,9 @@ public class CHEFBARBER {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int times = sc.nextInt();
+        int tests = sc.nextInt();
         int i = 0;
-        while (i < times){
+        while (i < tests){
             int a = sc.nextInt();
             int b = sc.nextInt();
             int result = a * b;
