43c15cab8d7cc08f994b9a1e7d101c973b4e0c02
Iuri Gabriel <iuri_gabriel@hotmail.com>
2022-03-20 15:31:43 -0300

    update!

---
+++ src/ICL1902.java
@@ -0,0 +1,23 @@
+//https://www.codechef.com/problems/ICL1902
+
+import java.util.Scanner;
+
+public class ICL1902 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int rounds = sc.nextInt(); rounds > 0; rounds--) {
+            int nCircles = sc.nextInt();
+            int result = 0;
+
+            while (nCircles > 0){
+                int squareRoot = (int) Math.sqrt(nCircles);
+                nCircles -= Math.pow(squareRoot, 2);
+                result++;
+            }
+
+            System.out.println(result);
+        }
+    }
+}
